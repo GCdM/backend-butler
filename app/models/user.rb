26c13ba3f7214f4af7566778aefe0de0fc7ffa52
GCdM <gabrielcm.br@gmail.com>
2018-09-11 10:52:36 +0100

    Changed serializers to consider household memebers and shift some information into UserInfo

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :payments
   has_many :event_users
   has_many :events, through: :event_users
+  has_many :responsibility_users
+  has_many :responsibilities, through: :responsibility_users
 
   validates :username, uniqueness: true
 end
